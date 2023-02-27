c8962a8bde7e3e74522e46ae9d82120c0204c3be
Rômulo Jales <romulo@thisismatter.com>
2023-02-27 15:36:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# matter-exceptions
+
+[![PyPI - Version](https://img.shields.io/pypi/v/matter-exceptions.svg)](https://pypi.org/project/matter-exceptions)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/matter-exceptions.svg)](https://pypi.org/project/matter-exceptions)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install matter-exceptions
+```
+
+## License
+
+`matter-exceptions` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ matter_exceptions/__init__.py
@@ -0,0 +1,33 @@
+__all__ = ["BaseAPIException", "DetailedException", "AuthenticationFailedError"]
+
+from http.client import HTTPException
+
+
+class BaseAPIException(HTTPException):
+    def __init__(self, description, payload=None, type=None):
+        HTTPException.__init__(self, description)
+
+        if payload:
+            self.data = payload
+        else:
+            self.data = {}
+
+        self.data["type"] = type
+        self.data["message"] = description
+        self.data["success"] = False
+
+
+class DetailedException(Exception):
+    TOPIC = "Error"
+
+    def __init__(self, message, data=None):
+        self.message = f"{self.TOPIC}: {message}" if not self.TOPIC in message else message
+        self.data = data
+        self.type = self.TOPIC.replace(" ", "_").lower()
+
+    def __reduce__(self):
+        return type(self), (self.message, self.data, )
+
+
+class AuthenticationFailedError(DetailedException):
+    TOPIC = "Authentication Failed Error"
